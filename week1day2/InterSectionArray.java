f152812639d18298df7a0c635cfa2837534f32d2
Karthick E <83011744+Karthick3012@users.noreply.github.com>
2021-10-07 15:57:31 +0530

    Add files via upload

---
+++ week1day2/InterSectionArray.java
@@ -0,0 +1,19 @@
+package week1day2;
+
+public class InterSectionArray {
+
+	public static void main(String[] args) {
+		
+		int arr1[] = {3,2,11,4,6,7};
+		int arr2[] = {1,2,8,4,9,7};
+		int length1 = arr1.length;
+		int length2 = arr2.length;
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr2.length;j++) {
+				if(arr1[i]==arr2[j]) {
+					System.out.println(arr1[i]);
+				}
+			}
+		}
+	}
+}
